b1e3506707de97d2ef4e4b9df161b47d604f8763
Murillo Goulart <murillo.cg@gmail.com>
2026-05-25 07:50:19 -0300

    Increase ECS to 1 vCPU / 2GB for photo processing

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -43,13 +43,13 @@ variable "db_username" {
 variable "ecs_cpu" {
   description = "ECS task CPU units"
   type        = number
-  default     = 512
+  default     = 1024
 }
 
 variable "ecs_memory" {
   description = "ECS task memory (MiB)"
   type        = number
-  default     = 1024
+  default     = 2048
 }
 
 variable "ecs_desired_count" {
