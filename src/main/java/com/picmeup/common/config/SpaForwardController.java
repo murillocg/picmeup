1ab938e9ca4efd8e1cd302f30e16858f4894e426
Murillo Goulart <murillo.cg@gmail.com>
2026-08-14 23:13:59 -0300

    Fix error handler to support all HTTP methods

--- src/main/java/com/picmeup/common/config/SpaForwardController.java
+++ src/main/java/com/picmeup/common/config/SpaForwardController.java
@@ -3,7 +3,7 @@ package com.picmeup.common.config;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.http.HttpServletRequest;
@@ -11,7 +11,7 @@ import jakarta.servlet.http.HttpServletRequest;
 @Controller
 public class SpaForwardController implements ErrorController {
 
-    @GetMapping("/error")
+    @RequestMapping("/error")
     public String handleError(HttpServletRequest request) {
         Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
         if (status != null && Integer.parseInt(status.toString()) == HttpStatus.NOT_FOUND.value()) {
